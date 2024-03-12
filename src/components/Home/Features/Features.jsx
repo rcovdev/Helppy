689f8763b0f717d1982e30e45a49d60124b178f3
rcovdev <rcovdev@gmail.com>
2024-03-12 12:03:49 -0600

    v.0.6.

--- src/components/Home/Features/Features.jsx
+++ src/components/Home/Features/Features.jsx
@@ -1,17 +1,45 @@
 import "./Features.scss";
-import features from "../../../../public/features.png"
+import features from "../../../../public/features.jpg"
+import { FaRegCheckCircle } from "react-icons/fa";
 
 const Features = () => {
     return (
         <section className="features">
             <div className="container">
                 <div className="textContainer">
-                    <h2>UNA CANTIDAD DE SERVICIOS INCOMPARABLE EN LA PALMA DE TU MANO.</h2>
-                    <div className="checkbox"></div>
+                    <h2>AN INCOMPARABLE AMOUNT OF SERVICES IN THE PALM OF YOUR HAND.</h2>
+                    <div className="item">
+                        <FaRegCheckCircle className="icon" />
+                        Affordable and reliable.
+                    </div>
+                    <p>
+                        Get crystal-clear pricing. Forget hourly rates, our services are delivered at fixed project costs.
+                    </p>
+                    <div className="item">
+                        <FaRegCheckCircle className="icon" />
+                        Don't sacrifice quality for speed. Get both.
+                    </div>
+                    <p>
+                        Stress-free project handoff, long-term value.
+                    </p>
+                    <div className="item">
+                        <FaRegCheckCircle className="icon" />
+                        Mutual satisfaction, secure payments.
+                    </div>
+                    <p>
+                        Transparent pricing, secure payments. Funds released when both parties are satisfied.
+                    </p>
+                    <div className="item">
+                        <FaRegCheckCircle className="icon" />
+                            Our expert support is available 24/7.
+                    </div>
+                    <p>
+                        Leverage the expertise of our 24/7 support team, available to resolve issues promptly, anytime and anywhere.
+                    </p>
                     
                 </div>
                 <div className="imgContainer">
-                    <img src={features} alt="" />
+                    <img src={features} alt="features_img" />
                 </div>
             </div>
         </section>
