34e7d0cba63f8326dae728623e8bdd656dc6feae
rcovdev <rcovdev@gmail.com>
2024-03-14 14:46:30 -0600

    v.0.7. (slider approved)

--- src/components/Home/Slide/Slide.jsx
+++ src/components/Home/Slide/Slide.jsx
@@ -1,51 +1,12 @@
 import "./Slide.scss";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+import { Slider } from "infinite-react-carousel";
 
 const Slide = ({children, slidesToShow, centerMode}) => {
-
-    let settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 5,
-        initalSlide: 0,
-        centerMode: true,
-        draggable: false,
-        lazyLoad: true,
-        nextArrow: <SampleNextArrow />,
-        prevArrow: <SamplePrevArrow />,
-        responsive: []
-    }
-
-    function SampleNextArrow(props) {
-        const { className, style, onClick } = props;
-        return (
-            <div
-                className={className}
-                onClick={onClick}
-                style={{backgroundColor: "red"}}
-            />
-        )
-
-    }
-    function SamplePrevArrow(props) {
-        const { className, style, onClick } = props;
-        return (
-            <div
-                className={className}
-                onClick={onClick}
-                style={{backgroundColor: "red"}}
-            />
-        )
-    }
-
     return (
         <section className="slide">
             <div className="container">
                 {/* SLIDER */}
-                <Slider {...settings}>
+                <Slider slidesToShow={slidesToShow} centerMode={centerMode}>
                     {children}
                 </Slider>
             </div>
