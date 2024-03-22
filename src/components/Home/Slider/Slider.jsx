2fbee32a7d36130b9a6f0f0efe57e5e3eafb4d65
rcovdev <rcovdev@gmail.com>
2024-03-22 16:56:41 -0600

    i fucking did it

---
+++ src/components/Home/Slider/Slider.jsx
@@ -0,0 +1,72 @@
+import "./Slider.scss";
+import LeftArrow from "../../../../public/LeftArrow.svg";
+import RightArrow from "../../../../public/RightArrow.svg";
+import { useRef } from "react";
+import { useState } from "react";
+
+const Slider = () => {
+    const [currentSlide, setCurrentSlide] = useState(1);
+    const SLIDER_REF = useRef();
+
+    const nextSlide = () => {
+        if (currentSlide < 3) {
+            setCurrentSlide(currentSlide + 1);
+        }
+    }
+        
+        const prevSlide = () => {
+            if (currentSlide > 1) {
+                setCurrentSlide(currentSlide - 1);
+            }
+        }
+
+        console.log(currentSlide)
+        
+    return (
+        <section className="slider">
+            <div className="container">
+                    <button className="prev-button" onClick={prevSlide}><img src={LeftArrow} alt="previous-slide-button" /></button>
+                <div className="wrapper">
+                    <div className="slider-track" ref={SLIDER_REF}>
+                        <div className="card">
+                            1
+                        </div>
+                        <div className="card">
+                            2
+                        </div>
+                        <div className="card">
+                            3
+                        </div>
+                        <div className="card">
+                            4
+                        </div>
+                        <div className="card">
+                            5
+                        </div>
+                        <div className="card">
+                            6
+                        </div>
+                        <div className="card">
+                            7
+                        </div>
+                        <div className="card">
+                            8
+                        </div>
+                        <div className="card">
+                            9
+                        </div>
+                        <div className="card">
+                            10
+                        </div>
+                        <div className="card">
+                            11
+                        </div>
+                    </div>
+                    <button className="next-button" onClick={nextSlide}><img src={RightArrow} alt="next-slide-button" /></button>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Slider
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@
 import "./Home.scss";
 // IMPORT COMPONENTS
 import Featured from "../../components/Home/Featured/Featured";
+import Slider from "../../components/Home/Slider/Slider";
 import Features from "../../components/Home/Features/Features";
 import Services from "../../components/Home/Services/Services";
 import Business from "../../components/Home/Business/Business";
@@ -12,7 +13,7 @@ const Home = () => {
             {/* FEATURED COMPONENT */}
             <Featured />
             {/* CATEGORY SLIDER COMPONENT */}
-            
+            <Slider />
             {/* FEATURES COMPONENT */}
             <Features />
             {/* OUR SERVICES */}
