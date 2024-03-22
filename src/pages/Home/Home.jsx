219ed92d69e5257e3ea3fdf80e069ab8b1f04f2f
rcovdev <rcovdev@gmail.com>
2024-03-22 12:07:41 -0600

    slider full remake

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,9 @@
 import "./Home.scss";
 // IMPORT COMPONENTS
 import Featured from "../../components/Home/Featured/Featured";
-import SlideShow from "../../components/Home/SlideShow/SlideShow";
 import Features from "../../components/Home/Features/Features";
 import Services from "../../components/Home/Services/Services";
 import Business from "../../components/Home/Business/Business";
-import Slider from "../../components/Home/Slider/Slider"
 
 const Home = () => {
     return (
@@ -14,7 +12,7 @@ const Home = () => {
             {/* FEATURED COMPONENT */}
             <Featured />
             {/* CATEGORY SLIDER COMPONENT */}
-            <Slider />
+            
             {/* FEATURES COMPONENT */}
             <Features />
             {/* OUR SERVICES */}
